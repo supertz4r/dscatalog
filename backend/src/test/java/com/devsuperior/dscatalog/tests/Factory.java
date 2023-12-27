ae22dedbbff5b3197b25c55515c8959731503147
supertz4r <juninsoader@gmail.com>
2023-12-27 11:36:02 -0300

    ProductServiceTests

--- backend/src/test/java/com/devsuperior/dscatalog/tests/Factory.java
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/Factory.java
@@ -10,12 +10,17 @@ public class Factory {
 
 	public static Product createProduct() {
 		Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2023-10-20T23:08:00Z"));
-		product.getCategories().add(new Category(2L, "Electronics"));
+		product.getCategories().add(createCategory());
 		return product;
+	}
+	
+	public static Category createCategory() {
+		return new Category(1L, "Electronics");
 	}
 	
 	public static ProductDTO createProductDTO() {
 		Product product = createProduct();
 		return new ProductDTO(product, product.getCategories());
 	}
+	
 }
